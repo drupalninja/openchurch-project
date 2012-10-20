b4100466cb520708357171126bb9918af9f2b421
jrcallicott <jrcallicott@192.168.11.105>
2012-10-20 10:50:24 -0500

    http://drupal.org/node/1785702
    -Fixing rounded corners

--- themes/openchurch_mm/js/openchurch-script.js
+++ themes/openchurch_mm/js/openchurch-script.js
@@ -1,9 +1,11 @@
-Drupal.behaviors.openchurchEqualheights = function (context) {
-  //adding equal heights to content-inner
-  if (jQuery().equalHeights) {
-    $("#content-inner div.equal-heights div.content").equalHeights();
-  }
+(function($) {
+  $(document).ready(function(){
+      //adding equal heights to content-inner
+    if (jQuery().equalHeights) {
+      $("#content-inner div.equal-heights div.content").equalHeights();
+    }
 
-  //adds rounded corners via js for IE
-  $('.rounded-corners .inner, .rounded-corners .pane-content').corner(".3em");
-}
+    //adds rounded corners via js for IE
+    $('.rounded-corners .inner, .rounded-corners .pane-content').corner(".3em");
+  });
+})(jQuery); 
