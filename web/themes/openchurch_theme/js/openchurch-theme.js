22282d7fa2dd4b296b3a1994601dd81471024c6a
Jay Callicott <jay.callicott@mediacurrent.com>
2023-01-02 13:27:42 -0600

    Updated navbar

--- web/themes/openchurch_theme/js/openchurch-theme.js
+++ web/themes/openchurch_theme/js/openchurch-theme.js
@@ -10,10 +10,12 @@
     attach: function (context, settings) {
       document.addEventListener("scroll", (event) => {
         let scroll = window.scrollY;
-        if (scroll > 30) {
+        if (scroll > 50) {
           document.querySelector('.sticky-top').classList.add('scrolled');
+          document.querySelector('.toolbar-tab .toolbar-tray-horizontal').classList.remove('is-active');
         } else {
           document.querySelector('.sticky-top').classList.remove('scrolled');
+          document.querySelector('.toolbar-tab .toolbar-tray-horizontal').classList.add('is-active');
         }
       });
     }
