9096795ef175ba98d1063a4a9829f8f008863d0c
Jay Callicott <jay.callicott@mediacurrent.com>
2023-01-02 12:51:45 -0600

    Updated navbar

--- web/themes/openchurch_theme/js/openchurch-theme.js
+++ web/themes/openchurch_theme/js/openchurch-theme.js
@@ -8,7 +8,14 @@
 
   Drupal.behaviors.openchurchTheme = {
     attach: function (context, settings) {
-
+      document.addEventListener("scroll", (event) => {
+        let scroll = window.scrollY;
+        if (scroll > 30) {
+          document.querySelector('.sticky-top').classList.add('scrolled');
+        } else {
+          document.querySelector('.sticky-top').classList.remove('scrolled');
+        }
+      });
     }
   };
 
