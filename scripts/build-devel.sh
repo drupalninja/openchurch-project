4d7d3a3ad103995165e22af683e00c5fe2acd935
Jay Callicott <jay.callicott@mediacurrent.com>
2023-01-16 08:18:12 -0600

    Added devel build script for testing

--- scripts/build-devel.sh
+++ scripts/build-devel.sh
@@ -3,5 +3,14 @@
 ## Install OpenChurch.
 ddev . drush si -y --site-name=OpenChurch && ddev . drush en -y openchurch_core && ddev . drush theme:enable openchurch_theme && ddev . drush config-set -y system.theme default openchurch_theme
 
+## Delete all nodes.
+ddev . drush entity:delete node
+
+## Enable devel and devel_generate.
+ddev . drush en -y devel devel_generate
+
+## Create dummy content for testing.
+ddev . drush devel-generate:content 100
+
 ## Output the login link.
 ddev . drush uli
