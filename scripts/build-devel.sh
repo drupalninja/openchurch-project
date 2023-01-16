4486fc0be943a393858fb40358cac938e8d1a1ba
Jay Callicott <jay.callicott@mediacurrent.com>
2023-01-16 08:24:53 -0600

    Tweaked devel scripts

--- scripts/build-devel.sh
+++ scripts/build-devel.sh
@@ -3,11 +3,14 @@
 ## Install OpenChurch.
 ddev . drush si -y --site-name=OpenChurch && ddev . drush en -y openchurch_core && ddev . drush theme:enable openchurch_theme && ddev . drush config-set -y system.theme default openchurch_theme
 
-## Delete all nodes.
-ddev . drush entity:delete node
-
 ## Enable devel and devel_generate.
 ddev . drush en -y devel devel_generate
+
+## Create terms.
+ddev . drush devel-generate:terms --bundles=tags 100
+
+## Create dummy menus for testing.
+ddev . drush devel-generate:menus 10
 
 ## Create dummy content for testing.
 ddev . drush devel-generate:content 100
